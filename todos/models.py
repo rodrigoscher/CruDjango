5fee1b686d2af4fffce5e2ad5915ff6e01b33fcd
Rodrigo Scher <79314511+rodrigoscher@users.noreply.github.com>
2024-02-21 20:33:56 -0300

    update

--- todos/models.py
+++ todos/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Comunicação com BD. herdando Model de models, importada acima
 class Todo(models.Model):
     # campos do TO DO. Traz os tipos de campos e argumentos para configuração
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(
+        verbose_name="Título", max_length=100, null=False, blank=False
+    )
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=True)
+    deadline = models.DateField(verbose_name="Data de entrega", null=True)
+    finished_at = models.DateField(null=True)
